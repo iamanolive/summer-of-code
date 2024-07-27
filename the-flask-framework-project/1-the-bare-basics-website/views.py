9946893b1cbcc4bed2a2bd196f11043084f7cce8
anoushka <theolivequeen.xyz@gmail.com>
2024-07-27 13:00:49 +0300

    bare basics website

--- the-flask-framework-project/1-the-bare-basics-website/views.py
+++ the-flask-framework-project/1-the-bare-basics-website/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
+from flask import jsonify
+from flask import redirect, url_for
 
 # initializing the blueprint
 views = Blueprint(__name__, "views")
@@ -8,6 +11,38 @@ views = Blueprint(__name__, "views")
 def home():
     return render_template("index.html", name = "winnie")
 
-@views.route("/profile/<username>")
-def profile(username):
-    return render_template("index.html", name = username)
+
+# @views.route("/profile/<username>")
+# def profile(username):
+#     return render_template("index.html", name = username)
+
+@views.route("/user_profile")
+def profile():
+    return render_template("profile.html")
+
+
+# query parameters
+# @views.route("/user_profile")
+# def profile():
+#     args = request.args
+#     name = args.get("name")
+#     return render_template("index.html", name = name)
+
+
+# return json data
+@views.route("/json")
+def get_json():
+    return jsonify({"name": "winnie", "coolness": 100})
+
+
+# getting data from incoming request
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+
+# redirecting
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("views.home"))
